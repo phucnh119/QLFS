2a2f107a4126e34566c39bfec54955ad54076be6
phucnh119 <phucnh1191988@gmail.com>
2018-09-06 16:32:18 +0700

    bỏ nút tìm kiếm

--- QLFS_Test1/QLFS_Test1/frm_Staff.Designer.cs
+++ QLFS_Test1/QLFS_Test1/frm_Staff.Designer.cs
@@ -43,7 +43,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dg_Show)).BeginInit();
             this.SuspendLayout();
             // 
@@ -205,21 +204,11 @@
             this.label5.TabIndex = 27;
             this.label5.Text = "Tìm Kiếm";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(942, 256);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
-            this.button1.TabIndex = 28;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // frm_Staff
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1300, 635);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.cmd_Exit);
@@ -260,6 +249,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
     }
 }
