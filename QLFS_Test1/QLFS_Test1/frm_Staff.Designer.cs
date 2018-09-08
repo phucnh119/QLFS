c2f2402623d908d42a136ef7218cb641beec8da9
Phuc <phucnh1191988@gmail.com>
2018-09-08 13:28:45 +0700

    phucnh

--- QLFS_Test1/QLFS_Test1/frm_Staff.Designer.cs
+++ QLFS_Test1/QLFS_Test1/frm_Staff.Designer.cs
@@ -51,9 +51,10 @@
             this.cmd_Exit.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmd_Exit.BackColor = System.Drawing.Color.DarkGray;
             this.cmd_Exit.ForeColor = System.Drawing.Color.Black;
-            this.cmd_Exit.Location = new System.Drawing.Point(871, 309);
+            this.cmd_Exit.Location = new System.Drawing.Point(1306, 452);
+            this.cmd_Exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_Exit.Name = "cmd_Exit";
-            this.cmd_Exit.Size = new System.Drawing.Size(85, 33);
+            this.cmd_Exit.Size = new System.Drawing.Size(128, 48);
             this.cmd_Exit.TabIndex = 25;
             this.cmd_Exit.Text = "Thoát";
             this.cmd_Exit.UseVisualStyleBackColor = false;
@@ -63,9 +64,10 @@
             this.cmd_Save.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmd_Save.BackColor = System.Drawing.Color.DarkGray;
             this.cmd_Save.ForeColor = System.Drawing.Color.Black;
-            this.cmd_Save.Location = new System.Drawing.Point(750, 309);
+            this.cmd_Save.Location = new System.Drawing.Point(1125, 452);
+            this.cmd_Save.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_Save.Name = "cmd_Save";
-            this.cmd_Save.Size = new System.Drawing.Size(85, 33);
+            this.cmd_Save.Size = new System.Drawing.Size(128, 48);
             this.cmd_Save.TabIndex = 24;
             this.cmd_Save.Text = "Lưu";
             this.cmd_Save.UseVisualStyleBackColor = false;
@@ -75,9 +77,10 @@
             this.cmd_Delete.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmd_Delete.BackColor = System.Drawing.Color.DarkGray;
             this.cmd_Delete.ForeColor = System.Drawing.Color.Black;
-            this.cmd_Delete.Location = new System.Drawing.Point(618, 309);
+            this.cmd_Delete.Location = new System.Drawing.Point(927, 452);
+            this.cmd_Delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_Delete.Name = "cmd_Delete";
-            this.cmd_Delete.Size = new System.Drawing.Size(85, 33);
+            this.cmd_Delete.Size = new System.Drawing.Size(128, 48);
             this.cmd_Delete.TabIndex = 23;
             this.cmd_Delete.Text = "Xóa";
             this.cmd_Delete.UseVisualStyleBackColor = false;
@@ -87,9 +90,10 @@
             this.cmd_Update.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmd_Update.BackColor = System.Drawing.Color.DarkGray;
             this.cmd_Update.ForeColor = System.Drawing.Color.Black;
-            this.cmd_Update.Location = new System.Drawing.Point(464, 309);
+            this.cmd_Update.Location = new System.Drawing.Point(696, 452);
+            this.cmd_Update.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_Update.Name = "cmd_Update";
-            this.cmd_Update.Size = new System.Drawing.Size(85, 33);
+            this.cmd_Update.Size = new System.Drawing.Size(128, 48);
             this.cmd_Update.TabIndex = 22;
             this.cmd_Update.Text = "Sửa";
             this.cmd_Update.UseVisualStyleBackColor = false;
@@ -99,9 +103,10 @@
             this.cmd_Add.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cmd_Add.BackColor = System.Drawing.Color.DarkGray;
             this.cmd_Add.ForeColor = System.Drawing.Color.Black;
-            this.cmd_Add.Location = new System.Drawing.Point(340, 309);
+            this.cmd_Add.Location = new System.Drawing.Point(510, 452);
+            this.cmd_Add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmd_Add.Name = "cmd_Add";
-            this.cmd_Add.Size = new System.Drawing.Size(85, 33);
+            this.cmd_Add.Size = new System.Drawing.Size(128, 48);
             this.cmd_Add.TabIndex = 21;
             this.cmd_Add.Text = "Thêm";
             this.cmd_Add.UseVisualStyleBackColor = false;
@@ -110,35 +115,39 @@
             // 
             this.dg_Show.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dg_Show.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dg_Show.Location = new System.Drawing.Point(28, 371);
+            this.dg_Show.Location = new System.Drawing.Point(42, 542);
+            this.dg_Show.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dg_Show.Name = "dg_Show";
             this.dg_Show.RowTemplate.Height = 24;
-            this.dg_Show.Size = new System.Drawing.Size(1245, 225);
+            this.dg_Show.Size = new System.Drawing.Size(1868, 329);
             this.dg_Show.TabIndex = 20;
             // 
             // cbo_IDBranch
             // 
             this.cbo_IDBranch.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cbo_IDBranch.FormattingEnabled = true;
-            this.cbo_IDBranch.Location = new System.Drawing.Point(589, 216);
+            this.cbo_IDBranch.Location = new System.Drawing.Point(884, 316);
+            this.cbo_IDBranch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbo_IDBranch.Name = "cbo_IDBranch";
-            this.cbo_IDBranch.Size = new System.Drawing.Size(273, 21);
+            this.cbo_IDBranch.Size = new System.Drawing.Size(408, 27);
             this.cbo_IDBranch.TabIndex = 19;
             // 
             // txt_FullName
             // 
             this.txt_FullName.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txt_FullName.Location = new System.Drawing.Point(589, 190);
+            this.txt_FullName.Location = new System.Drawing.Point(884, 278);
+            this.txt_FullName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_FullName.Name = "txt_FullName";
-            this.txt_FullName.Size = new System.Drawing.Size(273, 20);
+            this.txt_FullName.Size = new System.Drawing.Size(408, 26);
             this.txt_FullName.TabIndex = 18;
             // 
             // txt_IDStaff
             // 
             this.txt_IDStaff.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txt_IDStaff.Location = new System.Drawing.Point(589, 164);
+            this.txt_IDStaff.Location = new System.Drawing.Point(884, 240);
+            this.txt_IDStaff.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_IDStaff.Name = "txt_IDStaff";
-            this.txt_IDStaff.Size = new System.Drawing.Size(273, 20);
+            this.txt_IDStaff.Size = new System.Drawing.Size(408, 26);
             this.txt_IDStaff.TabIndex = 17;
             // 
             // label4
@@ -146,9 +155,10 @@
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(422, 219);
+            this.label4.Location = new System.Drawing.Point(633, 320);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.Size = new System.Drawing.Size(117, 19);
             this.label4.TabIndex = 16;
             this.label4.Text = "Đơn Vị Công Tác";
             // 
@@ -157,9 +167,10 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(422, 193);
+            this.label3.Location = new System.Drawing.Point(633, 282);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(55, 19);
             this.label3.TabIndex = 15;
             this.label3.Text = "Họ Tên";
             // 
@@ -168,9 +179,10 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(422, 167);
+            this.label2.Location = new System.Drawing.Point(633, 244);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(98, 19);
             this.label2.TabIndex = 14;
             this.label2.Text = "Mã Nhân Viên";
             // 
@@ -181,34 +193,37 @@
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(28, -3);
+            this.label1.Location = new System.Drawing.Point(42, -4);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1245, 74);
+            this.label1.Size = new System.Drawing.Size(1868, 108);
             this.label1.TabIndex = 13;
             this.label1.Text = "Quản Lý Nhân Viên";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(589, 259);
+            this.textBox1.Location = new System.Drawing.Point(884, 379);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.Size = new System.Drawing.Size(408, 26);
             this.textBox1.TabIndex = 26;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(436, 262);
+            this.label5.Location = new System.Drawing.Point(654, 383);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.Size = new System.Drawing.Size(69, 19);
             this.label5.TabIndex = 27;
             this.label5.Text = "Tìm Kiếm";
             // 
             // frm_Staff
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 635);
+            this.ClientSize = new System.Drawing.Size(1362, 741);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.cmd_Exit);
@@ -224,8 +239,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_Staff";
-            this.Text = "frm_Staff";
+            this.Text = "Quản lý nhân viên";
             ((System.ComponentModel.ISupportInitialize)(this.dg_Show)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
