1cbde2575cf5ff57ebc809c4c939c240079ad9d0
phucnh119 <phucnh1191988@gmail.com>
2018-09-11 09:59:28 +0700

    design form Account

--- QLFS_Test1/QLFS_Test1/frm_Account.Designer.cs
+++ QLFS_Test1/QLFS_Test1/frm_Account.Designer.cs
@@ -28,21 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbo_Menulist = new System.Windows.Forms.ComboBox();
+            this.dg_Show = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmd_Them = new System.Windows.Forms.Button();
+            this.cmd_Update = new System.Windows.Forms.Button();
+            this.cmd_Save = new System.Windows.Forms.Button();
+            this.cmd_Delete = new System.Windows.Forms.Button();
+            this.cmd_Exit = new System.Windows.Forms.Button();
+            this.cbo_FullName = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_Show)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÝ NGƯỜI DÙNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tài Khoản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Họ tên";
+            // 
+            // txt_User
+            // 
+            this.txt_User.Location = new System.Drawing.Point(188, 75);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(378, 26);
+            this.txt_User.TabIndex = 4;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(188, 108);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(378, 26);
+            this.txt_Password.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Phần Quyền Hệ Thống";
+            // 
+            // cbo_Menulist
+            // 
+            this.cbo_Menulist.FormattingEnabled = true;
+            this.cbo_Menulist.Location = new System.Drawing.Point(188, 174);
+            this.cbo_Menulist.Name = "cbo_Menulist";
+            this.cbo_Menulist.Size = new System.Drawing.Size(378, 27);
+            this.cbo_Menulist.TabIndex = 8;
+            // 
+            // dg_Show
+            // 
+            this.dg_Show.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_Show.Location = new System.Drawing.Point(596, 75);
+            this.dg_Show.Name = "dg_Show";
+            this.dg_Show.Size = new System.Drawing.Size(592, 159);
+            this.dg_Show.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 208);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tìm Kiếm";
+            // 
+            // cmd_Them
+            // 
+            this.cmd_Them.Location = new System.Drawing.Point(188, 253);
+            this.cmd_Them.Name = "cmd_Them";
+            this.cmd_Them.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Them.TabIndex = 13;
+            this.cmd_Them.Text = "Thêm";
+            this.cmd_Them.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Update
+            // 
+            this.cmd_Update.Location = new System.Drawing.Point(270, 253);
+            this.cmd_Update.Name = "cmd_Update";
+            this.cmd_Update.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Update.TabIndex = 14;
+            this.cmd_Update.Text = "Sửa";
+            this.cmd_Update.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Save
+            // 
+            this.cmd_Save.Location = new System.Drawing.Point(352, 253);
+            this.cmd_Save.Name = "cmd_Save";
+            this.cmd_Save.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Save.TabIndex = 15;
+            this.cmd_Save.Text = "Lưu";
+            this.cmd_Save.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Delete
+            // 
+            this.cmd_Delete.Location = new System.Drawing.Point(434, 253);
+            this.cmd_Delete.Name = "cmd_Delete";
+            this.cmd_Delete.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Delete.TabIndex = 16;
+            this.cmd_Delete.Text = "Xóa";
+            this.cmd_Delete.UseVisualStyleBackColor = true;
+            // 
+            // cmd_Exit
+            // 
+            this.cmd_Exit.Location = new System.Drawing.Point(516, 253);
+            this.cmd_Exit.Name = "cmd_Exit";
+            this.cmd_Exit.Size = new System.Drawing.Size(75, 23);
+            this.cmd_Exit.TabIndex = 17;
+            this.cmd_Exit.Text = "Thoát";
+            this.cmd_Exit.UseVisualStyleBackColor = true;
+            // 
+            // cbo_FullName
+            // 
+            this.cbo_FullName.FormattingEnabled = true;
+            this.cbo_FullName.Location = new System.Drawing.Point(188, 141);
+            this.cbo_FullName.Name = "cbo_FullName";
+            this.cbo_FullName.Size = new System.Drawing.Size(378, 27);
+            this.cbo_FullName.TabIndex = 18;
             // 
             // frm_Account
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.cbo_FullName);
+            this.Controls.Add(this.cmd_Exit);
+            this.Controls.Add(this.cmd_Delete);
+            this.Controls.Add(this.cmd_Save);
+            this.Controls.Add(this.cmd_Update);
+            this.Controls.Add(this.cmd_Them);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dg_Show);
+            this.Controls.Add(this.cbo_Menulist);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frm_Account";
             this.Text = "Quản lý người dùng";
+            ((System.ComponentModel.ISupportInitialize)(this.dg_Show)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbo_Menulist;
+        private System.Windows.Forms.DataGridView dg_Show;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button cmd_Them;
+        private System.Windows.Forms.Button cmd_Update;
+        private System.Windows.Forms.Button cmd_Save;
+        private System.Windows.Forms.Button cmd_Delete;
+        private System.Windows.Forms.Button cmd_Exit;
+        private System.Windows.Forms.ComboBox cbo_FullName;
     }
 }
